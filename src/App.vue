28cd5082ddcc60946930dfc20cd7d884093d2b5e
unknown <bm.shadow@hotmail.com>
2021-05-07 03:56:11 +0300

    refixed css

--- src/App.vue
+++ src/App.vue
@@ -61,7 +61,9 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
 #app {
+  
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
