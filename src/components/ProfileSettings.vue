42d26580e891df519b3be1493c5d855cd2eb82df
unknown <bm.shadow@hotmail.com>
2021-06-29 01:41:55 +0300

    changed + added some css profile-setttings

--- src/components/ProfileSettings.vue
+++ src/components/ProfileSettings.vue
@@ -1,25 +1,25 @@
 <template>
     <div class="profile-page-settings-container">
         <div class="profile-page-settings-title">Details:</div>
-        <div class="profile-page-settings-profile-picture-div">Profile Picture: <img class="profile-page-settings-profile-picture" src="https://cdn.myanimelist.net/images/userimages/3936693.jpg?t=1624917000" alt="" srcset=""><input type="file" name="profile-pic"></div>
+        <div class="profile-page-settings-profile-picture-div">Profile Picture: <img class="profile-page-settings-profile-picture" src="https://cdn.myanimelist.net/images/userimages/3936693.jpg?t=1624917000" alt="" srcset=""><input class="profile-page-settings-input-image" type="file" name="profile-pic"></div>
         
         <div class="profile-page-settings-details">
-            <div>Gender: <select class="gender-selector" name="gender">
+            <div class="settings-detail">Gender: <select class="gender-selector" name="gender">
                 <option class="gender-selector-option" value="Male">Male</option>
                 <option class="gender-selector-option" value="Female">Female</option>
                 <option class="gender-selector-option" value="Other">Other</option>
             </select>
             </div>
-            <div>Birthday: <input type="date" class="date-selector" name="birthday"></div>
-            <div>Nationality: <Nationalities/></div>
-            <div>Location: </div>
-            <input class="profile-page-settings-input-text" type="text">
-            <div>First Anime: </div>
-            <input class="profile-page-settings-input-text" type="text">
-            <div>First Manga: </div>
-            <input class="profile-page-settings-input-text" type="text">
-            <div>Bio: </div>
-            <textarea name="bio" cols="30" rows="10"></textarea>
+            <div class="settings-detail">Birthday: <input type="date" class="date-selector" name="birthday"></div>
+            <div class="settings-detail">Nationality: <Nationalities/></div>
+            <div class="settings-detail">Location: <input class="profile-page-settings-input-text" type="text"></div>
+            
+            <div class="settings-detail">First Anime: <input class="profile-page-settings-input-text" type="text"></div>
+            
+            <div class="settings-detail">First Manga:  <input class="profile-page-settings-input-text" type="text"></div>
+           
+            <div class="settings-detail">Bio: <textarea name="bio" cols="30" rows="10"></textarea></div>
+            
 
         </div>
     </div>
