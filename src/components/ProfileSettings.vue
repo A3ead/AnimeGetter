6a90dfecd775c8fbc125746ea146952bc2de7be1
A3ead <to.abdelsalam@gmail.com>
2021-06-29 16:22:52 +0200

    added upload profile picture button functionality (no saving in storage yet) and added placeholder image

--- src/components/ProfileSettings.vue
+++ src/components/ProfileSettings.vue
@@ -2,8 +2,7 @@
     <div class="profile-page-settings-container">
         <div class="profile-page-settings-title">Details:</div>
         <div>Profile Picture:</div>
-        <div class="profile-page-settings-profile-picture-div"><img class="profile-page-settings-profile-picture" src="https://cdn.myanimelist.net/images/userimages/3936693.jpg?t=1624917000" alt="" srcset=""><input class="profile-page-settings-input-image" type="file" name="profile-pic"></div>
-        
+        <div class="profile-page-settings-profile-picture-div"><img class="profile-page-settings-profile-picture" :src="imgURL" alt="" srcset=""><div style="margin-left: 5px;"><input class="profile-page-settings-input-image" type="file" name="profile-pic" size="50" @change="changePP"></div></div>
         <div class="profile-page-settings-details">
             <div class="settings-detail">Gender: <select class="gender-selector" name="gender">
                 <option class="gender-selector-option" value="Male">Male</option>
@@ -37,13 +36,17 @@ export default {
   },
     data(){
     return{
+        imgURL:'https://cdn.discordapp.com/attachments/672949321032925185/859435116183027762/Untitled-1.png'
     }
   },
   mounted(){
-   
+      
   },
   methods:{
-
+      changePP(e){
+          let uploadImage = e.target.files[0]
+          this.imgURL = URL.createObjectURL(uploadImage)
+      }
       
   }
 }
